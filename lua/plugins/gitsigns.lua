788059b7a88667597900efd0c08f3ef103b0df3e
h.takakura <takakura@orinos.co.jp>
2024-07-07 01:35:27 +0900

    gitsignsの便利そうなキーマップ

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -22,6 +22,24 @@ return {
 						opts.buffer = bufnr
 						vim.keymap.set(mode, l, r, opts)
 					end
+
+					map("n", "]c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "]c", bang = true })
+						else
+							gitsigns.nav_hunk("next")
+						end
+					end)
+					map("n", "[c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "[c", bang = true })
+						else
+							gitsigns.nav_hunk("prev")
+						end
+					end)
+					map("n", "<leader>ba", function()
+						gitsigns.blame_line({ full = true })
+					end)
 					map("n", "<leader>bl", gitsigns.toggle_current_line_blame)
 					map("n", "<leader>hd", gitsigns.diffthis)
 				end,
