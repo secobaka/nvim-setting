ee8745bd51bd7693aa7810ac9f1e2f7321055820
h.takakura <takakura@orinos.co.jp>
2024-07-06 22:25:27 +0900

    こちゃこちゃ

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,40 +6,26 @@ return {
 			"BufRead",
 		},
 		opts = function()
-			require("gitsigns").setup({
+			return {
 				signs = {
-					add = {
-						hl = "GitSignsAdd",
-						text = " ▎",
-						numhl = "GitSignsAddNr",
-						linehl = "GitSignsAddLn",
-					},
-					change = {
-						hl = "GitSignsChange",
-						text = " ▎",
-						numhl = "GitSignsChangeNr",
-						linehl = "GitSignsChangeLn",
-					},
-					delete = {
-						hl = "GitSignsDelete",
-						text = " ",
-						numhl = "GitSignsDeleteNr",
-						linehl = "GitSignsDeleteLn",
-					},
-					topdelete = {
-						hl = "GitSignsDelete",
-						text = " ",
-						numhl = "GitSignsDeleteNr",
-						linehl = "GitSignsDeleteLn",
-					},
-					changedelete = {
-						hl = "GitSignsChange",
-						text = "▎ ",
-						numhl = "GitSignsChangeNr",
-						linehl = "GitSignsChangeLn",
-					},
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
 				},
-			})
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+					map("n", "<leader>bl", gitsigns.toggle_current_line_blame)
+					map("n", "<leader>hd", gitsigns.diffthis)
+				end,
+			}
 		end,
 	},
 }
--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -24,41 +24,6 @@ return {
 					Misc = { color = colors.purple },
 				},
 			})
-
-			require("gitsigns").setup({
-				signs = {
-					add = {
-						hl = "GitSignsAdd",
-						text = " ▎",
-						numhl = "GitSignsAddNr",
-						linehl = "GitSignsAddLn",
-					},
-					change = {
-						hl = "GitSignsChange",
-						text = " ▎",
-						numhl = "GitSignsChangeNr",
-						linehl = "GitSignsChangeLn",
-					},
-					delete = {
-						hl = "GitSignsDelete",
-						text = " ",
-						numhl = "GitSignsDeleteNr",
-						linehl = "GitSignsDeleteLn",
-					},
-					topdelete = {
-						hl = "GitSignsDelete",
-						text = " ",
-						numhl = "GitSignsDeleteNr",
-						linehl = "GitSignsDeleteLn",
-					},
-					changedelete = {
-						hl = "GitSignsChange",
-						text = "▎ ",
-						numhl = "GitSignsChangeNr",
-						linehl = "GitSignsChangeLn",
-					},
-				},
-			})
 			require("scrollbar.handlers.gitsigns").setup()
 
 			require("scrollbar.handlers.search").setup()
