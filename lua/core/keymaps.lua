c4c1fc0e91839aa12d4af0db20b11cd070c2ef38
h.takakura <takakura@orinos.co.jp>
2024-07-07 01:17:31 +0900

    ウィンドウ間の移動のkeymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n", "s", '"_s')
 
 -- ESC2回で検索を抜ける
 vim.keymap.set("n", "<ESC><ESC>", ":nohl<CR>")
+
+-- ウィンドウ間の移動
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
